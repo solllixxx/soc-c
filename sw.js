6536a9f050eab989b8cb121ad21142235415493c
solix <zazhytskyi1@gmail.com>
2026-05-11 23:08:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
-const CACHE_NAME = 'soc-messenger-v1';
+const CACHE_NAME = 'soc-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json'
+    '.',
+    'index.html',
+    'manifest.json'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
     );
 });
